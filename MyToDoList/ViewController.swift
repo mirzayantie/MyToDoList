8ff288d23ba6db110f27d989f93024cc0e4ea8f0
Mirzayantie <Mirzayantie>
2019-10-24 11:21:40 +0800

    Initial Commit

---
+++ MyToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyToDoList
+//
+//  Created by Mirzayantie on 24/10/2019.
+//  Copyright © 2019 Mirzayantie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
